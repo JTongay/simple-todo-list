31711abe26dc4c3dab1a8003cc13fd6f3458ebeb
Joseph Tongay <dirk.tongay@gmail.com>
2016-08-29 16:25:05 -0500

    fully functional yo

--- app.js
+++ app.js
@@ -1,102 +1,83 @@
-(function(doc) {
+(function() {
 
-    //Create text box --done
-    //Need to create ul and create li's on click of button
-    //set input type to text
-    //when you click on an li, it should add class .completed
-    //give submit button an id of #add_todo
+    $(function() {
+        //Create the title
+        //Create Text box
+        //Need to create ul and create li's on click or enter key of button
+        //set input type to text
+        //when you click on edit button, a textbox should appear with a button saying submit
+        //when you click on x button, it should delete the li
 
-    var body = doc.getElementsByTagName('body')[0]
-    var title = doc.createElement('h1')
-    var textBox = doc.createElement('input');
-    var inputForm = doc.createElement('form');
-    var subButton = doc.createElement('button');
-    var list = doc.createElement('ul');
-
-
-    //Creates Title
-    title.innerText = "Todo App"
-
-    //Sets form actions
-    inputForm.setAttribute("action", "index.html")
-    inputForm.setAttribute("method", "get")
-
-    //Creates Textbox
-    textBox.setAttribute("type", "text");
-    textBox.setAttribute("id", "textBox")
-    textBox.setAttribute("placeholder", "What to do?");
-
-    //Creates Submit Button
-    subButton.setAttribute("type", "submit");
-    subButton.setAttribute("id", "add_todo")
-    subButton.innerHTML = "Add";
+        var $body = $('body');
+        var $title = $('<h1>To-Do App</h1>');
+        var $form = $("<form>")
+        var $textBox = $("<input type='text' placeholder='What to do?' id='textBox'>");
+        var $subButton = $("<button type='submit' id='add_todo'>Add</button>")
+        var $taskList = $('<ul>');
 
 
-    //Events
-    subButton.addEventListener('click', function(event) {
-        var text = doc.getElementById('textBox').value;
 
-        //If block checks if textbox is empty
-        if (text !== "") {
-            //this keeps the page from refreshing
+
+        $body.append($title);
+        $form.append($textBox);
+        $form.append($subButton);
+        $body.append($form)
+        $body.append($taskList)
+
+
+        //Event Handlers
+        $(document)
+            .on('click', '#add_todo', addListItem)
+            .on('click', '.delete', deleteListItem)
+            .on('click', '.edit', editListItem)
+            .on('click', '.confirm', confirmEdit)
+
+
+        function addListItem(event) {
             event.preventDefault();
-            //these create the variables I need to use
-            var listItem = doc.createElement('li');
-            var removeButton = doc.createElement('button')
-            var edit = doc.createElement('button')
-                //list items
-            listItem.className = "tasks"
-            listItem.innerHTML = text
-                //remove button
-            removeButton.innerHTML = "X"
-            removeButton.className = "delete"
-            removeButton.setAttribute("type", "button")
-                //edit button
-            edit.innerHTML = "Edit";
-            edit.className = "edit";
-            //appends element to parent node
-            listItem.appendChild(edit)
-            listItem.appendChild(removeButton)
-            list.appendChild(listItem)
-                //sets textbox back to empty when submitted
-            doc.getElementById('textBox').value = "";
-            //remove button functionality
-            removeButton.addEventListener("click", function() {
-                    listItem.parentNode.removeChild(listItem)
-                })
-                //edit button functionality
-            edit.addEventListener('click', function() {
-                var editBox = "<input type='text' id='editBox' value=" + text + "><button id='update' type='submit'>Update</button>";
-
-                listItem.innerHTML = editBox;
-                var update = doc.getElementById("update")
-                update.addEventListener("click", function() {
-                    text = doc.getElementById("editBox").value;
-                    listItem.innerHTML = text;
-                    listItem.appendChild(edit);
-                    listItem.appendChild(removeButton)
-
-
-                })
-            })
-        } else {
-            event.preventDefault();
-            alert("You have to enter something to do!")
+            var $listItem = $("<li class='tasks'>");
+            var text = $textBox.val();
+            var $deleteBtn = $("<button class='delete'>X</button>")
+            var $editBth = $("<button class='edit'>Edit</button>")
+            if (text.trim().length === 0) {
+                alert("you gotta add something!")
+                return;
+            }
+            $listItem.html(text)
+            $textBox.val("")
+            $listItem.append($deleteBtn)
+            $listItem.append($editBth)
+            $taskList.append($listItem)
         }
+
+        function deleteListItem() {
+            $(this).parent().remove()
+        }
+
+        function editListItem() {
+            var $editBox = $("<input type='text' class='editBox' placeholder='Make Changes?'><button class='confirm'>Confirm</button>")
+
+            var listText = $(this).parent().html($editBox)
+
+        }
+
+        function confirmEdit() {
+            text = $('.editBox').val();
+            var $listItem = $("<li class='tasks'>");
+            var $deleteBtn = $("<button class='delete'>X</button>")
+            var $editBth = $("<button class='edit'>Edit</button>")
+            $listItem.html(text)
+            $listItem.append($deleteBtn)
+            $listItem.append($editBth)
+            $taskList.append($listItem)
+            $(this).parent().replaceWith($listItem)
+
+
+        }
+
+
+
+
+
     })
-
-
-    //Appends elements
-
-    body.appendChild(title)
-    inputForm.appendChild(textBox)
-    inputForm.appendChild(subButton)
-    body.appendChild(inputForm)
-
-    body.appendChild(list)
-
-
-
-
-
-})(document)
+})();
